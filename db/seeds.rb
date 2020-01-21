6fc51c6d63f1cecb618ee2a8d7aecf956ce39b1f
github username <github email address>
2020-01-21 11:08:50 -0600

    back end creation rails git

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# genres_list = [
+#     'Rock', 'Hip Hop', 'Blues', 'Pop', 'Metal', 'Jazz', 'Funk', 'Country', 'Classical', 'Punk', 'Folk', 'Rhythm and Blues', 'Reggae', 'Soul', 'Techno', 'House', 'Disco', 'EDM', 'Grunge', 'Indie', 'Ska'
+# ]
+
+# genres_list.each do |gen|
+#     Genre.create(name: gen)
+# end
+
+# artists_list = [
+#     'ABBA', 'AC/DC', 'Aerosmith', 'Albert Hammond', 'Albert Hammond Jr', 'Al Green', 'alt-J', 'Arcade Fire', 'Arctic Monkeys', 'Ariel Pink', 'Band of Skulls', 'Battle Tapes', 'the Beatles', 'Beck', 'the Black Keys', 'Black Pistol Fire', 'the Blaze', 'blink-182', 'Blur', 'Bob Dylan', 'Bob Moses', 'Bonobo', 'Cage the Elephant', 'Cake', 'Carpenter Brut', 'the Cars', 'the Clash', 'the Cure', 'Daft Punk', 'David Bowie', 'Dire Straits', 'the Doors', 'Eagles', 'Elliott Smith', 'Green Day', 'the Growlers', 'John Mayer', 'Julian Casablancas', 'the Strokes', 'Queens of the Stone Age', 'Strawberry Girls', 'Royal Blood', 'STRFKR'
+# ]
+
+# genres_list.each do |art|
+#     Artist.create(name: art)
+# end
